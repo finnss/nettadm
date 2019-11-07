618cbd54c5ca522edd75aaa4f814a0586f5a9669
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2019-11-07 18:57:46 +0100

    Add webapp for assignment2

---
+++ assignment2/server/server.py
@@ -0,0 +1,113 @@
+import xmltodict
+from flask import Flask, request, Response
+from flask_cors import CORS
+import os
+import json
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/")
+def index():
+    return "Hello"
+
+
+@app.route("/cim_info", methods=['GET'], strict_slashes=False)
+def parse_request():
+    command_to_get_OS = 'wbemcli ei http://ttm4128.item.ntnu.no:5988/root/cimv2/CIM_OperatingSystem -nl | grep Version'
+    # os_command_output = os.popen(command_to_get_OS).read()
+    os_command_output = "NAME=\"Ubuntu\" VERSION=\"14.04.6 LTS,Trusty Tahr\" ID=ubuntu ID_LIKE=debian PRETTY_NAME=\"Ubuntu 14.04.6 LTS\""
+
+    command_to_get_IP_names = 'wbemcli ein http://ttm4128.item.ntnu.no:5988/root/cimv2:CIM_IPProtocolEndpoint -nl | grep Name'
+    command_to_get_IP_addresses = 'wbemcli ein http://ttm4128.item.ntnu.no:5988/root/cimv2:CIM_IPProtocolEndpoint -nl | grep IPv4Address'
+    command_to_get_IP_mask = 'wbemcli ein http://ttm4128.item.ntnu.no:5988/root/cimv2:CIM_IPProtocolEndpoint -nl | grep subnet'
+    # ip_names_command_output = os.popen(command_to_get_IP).read()
+    # ip_addr_command_output = os.popen(command_to_get_IP).read()
+    # ip_mask_command_output = os.popen(command_to_get_IP).read()
+    ip_names_command_output = 'Name="IPv4_eth0"'
+    ip_addr_command_output = 'IPv4Address="129.241.200.173"'
+    ip_mask_command_output = 'SubnetMask="255.255.0.0"'
+
+    xml_data = \
+"""
+<MESSAGE>
+    <PROPERTY NAME="Version" TYPE="string">
+        <VALUE>{}</VALUE>
+    </PROPERTY>
+    <PROPERTY NAME="IpInterfaces">
+        <PROPERTY NAME="IpInterface0">
+            <PROPERTY NAME="Name" TYPE="string">
+                <VALUE>{}</VALUE>
+            </PROPERTY>
+            <PROPERTY NAME="IPv4Address" TYPE="string">
+                <VALUE>{}</VALUE>
+            </PROPERTY>
+            <PROPERTY NAME="SubnetMask" TYPE="string">
+                <VALUE>{}</VALUE>
+            </PROPERTY>
+        </PROPERTY>
+    </PROPERTY>
+</MESSAGE>
+""".format(os_command_output, ip_names_command_output, ip_addr_command_output, ip_mask_command_output)
+    # parsed_os = xmltodict.parse(os_xml_data)
+    # os_xml = xmltodict.unparse(parsed_os)
+
+    # ip_xml = xmltodict.unparse(xmltodict.parse(ip_xml_data)).replace('<?xml version="1.0" encoding="utf-8"?>', '')
+    # xml = xmltodict.unparse(xmltodict.parse(xml_data))
+    xml = '<?xml version="1.0" encoding="utf-8"?>\n' + xml_data
+    a = xmltodict.parse(xml_data)
+
+    """
+    xml_dict = {
+        'MESSAGE': {
+            'PROPERTY': {
+                '@NAME': 'Version',
+                '@TYPE': 'string',
+                'VALUE': os_command_output
+            },
+            'PROPERTY': {
+                '@NAME': 'IP_INTERFACES',
+                'VALUE': [
+                    {
+                        'PROPERTY': {
+                            '@NAME': 'Name',
+                            '@TYPE': 'string',
+                            'VALUE': ip_names_command_output
+                        },
+                        'PROPERTY': {
+                            '@NAME': 'IPv4Address',
+                            '@TYPE': 'string',
+                            'VALUE': ip_addr_command_output
+                        },
+                        'PROPERTY': {
+                            '@NAME': 'SubnetMask',
+                            '@TYPE': 'string',
+                            'VALUE': ip_mask_command_output
+                        },
+                    }
+                ]
+            }
+        }
+    }
+    xml_to_return = xmltodict.unparse(xml_dict)
+    """
+    return Response(xml, mimetype='application/xml')
+
+
+@app.route("/snmp_info", methods=['GET'], strict_slashes=False)
+def parse_snmp_request():
+    command_to_get_sysName = 'snmpgetnext -v 2c -c ttm4128 localhost sysDescr'
+    sys_name = os.popen(command_to_get_sysName).read()
+
+    command_to_get_IP_packets = 'snmpgetnext -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1.1'
+    ip_command_output = os.popen(command_to_get_IP_packets).read()
+
+    jsonResult = {
+        'os': sys_name,
+        'ipInterfaces': ip_command_output
+    }
+
+    return Response(json.dumps(jsonResult), mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ assignment2/webapp/src/App.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+const XMLParser = require("react-xml-parser");
+
+function App() {
+  const [cimData, setCimData] = useState({});
+  const [snmpData, setSnmpData] = useState({});
+  const [useCim, setUseCim] = useState(true);
+
+  useEffect(() => {
+    useCim
+      ? Object.keys(cimData).length === 0 &&
+        fetch("http://localhost:5000/cim_info")
+          .then(response => response.text())
+          .then(response => {
+            const xml = new XMLParser().parseFromString(response);
+            setCimData(xml);
+          })
+          .catch(err => {
+            console.log("fetch", err);
+          })
+      : Object.keys(snmpData).length === 0 &&
+        fetch("http://localhost:5000/snmp_info")
+          .then(response => response.json())
+          .then(response => {
+            setSnmpData(response);
+          })
+          .catch(err => {
+            console.log("fetch", err);
+          });
+  });
+
+  const osData =
+    cimData.children &&
+    cimData.children.find(child => child.attributes.NAME === "Version")
+      .children[0].value;
+  const ipData =
+    cimData.children &&
+    cimData.children.find(child => child.attributes.NAME === "IpInterfaces")
+      .children;
+
+  console.log("State:");
+  console.log("cimData", cimData);
+  console.log("snmpData", snmpData);
+
+  return (
+    <div className="App">
+      <header className="content">
+        <div className="inputDataSelector">
+          <div
+            className={`${useCim ? "selected" : ""}`}
+            onClick={() => setUseCim(true)}
+          >
+            CIM
+          </div>
+          <div
+            className={`${!useCim ? "selected" : ""}`}
+            onClick={() => setUseCim(false)}
+          >
+            SNMP
+          </div>
+        </div>
+
+        {useCim ? (
+          <div className="displayData">
+            <h1>CIM Data</h1>
+            <h2>OS</h2>
+            <div className="dataTable osTable">
+              {formatXMLOsData(osData || "")}
+            </div>
+
+            <h2>IP</h2>
+            <div className="dataTable ipTable">
+              {(ipData || []).map(ipInterface => {
+                return formatXMLIpInterface(ipInterface);
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="displayData">
+            <h1>SNMP Data</h1>
+            <h2>OS</h2>
+            <div className="dataTable osTable">
+              {formatJSONOsData(snmpData.os || "")}
+            </div>
+
+            <h2>IP</h2>
+            <div className="dataTable ipTable">{snmpData.ipInterfaces}</div>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+const formatXMLOsData = data => {
+  const parsed = parseDataWithVaryingTuples(data);
+
+  return Object.keys(parsed).map(key => {
+    const value = parsed[key];
+    return (
+      <div className="tableRow">
+        <span className="tableKey">{key}</span>
+        <span className="tableValue">{value}</span>
+      </div>
+    );
+  });
+};
+
+const formatXMLIpInterface = ipInterface => {
+  const parsed = {};
+
+  const name = ipInterface.children[0].children[0].value
+    .split("=")[1]
+    .replace('"', "")
+    .replace('"', "");
+
+  const ipAddr =
+    ipInterface.children[1] &&
+    ipInterface.children[1].children[0].value
+      .split("=")[1]
+      .replace('"', "")
+      .replace('"', "");
+
+  const subnetMask =
+    ipInterface.children[2] &&
+    ipInterface.children[2].children[0].value
+      .split("=")[1]
+      .replace('"', "")
+      .replace('"', "");
+
+  return (
+    <div>
+      <div className="tableRow">
+        <span className="tableKey">Name</span>
+        <span className="tableValue">IP Address</span>
+        <span className="tableValue">Subnet Mask</span>
+      </div>
+      <div className="tableRow">
+        <span className="tableKey">{name}</span>
+        <span className="tableValue">{ipAddr}</span>
+        <span className="tableValue">{subnetMask}</span>
+      </div>
+    </div>
+  );
+};
+
+const formatJSONOsData = osData => {
+  console.log("osData", osData);
+  return <div>{osData}</div>;
+};
+
+const formatJSONIpInterface = ipInterface => {
+  console.log("osData", ipInterface);
+  return <div>{ipInterface}</div>;
+};
+
+const parseDataWithVaryingTuples = data => {
+  if (!data) return {};
+
+  const splitIndexes = data
+    .split("")
+    .map((char, index) => (char === " " ? index : false))
+    .filter(Boolean);
+
+  const equalsIndexes = data
+    .split("")
+    .map((char, index) => (char === "=" ? index : false))
+    .filter(Boolean);
+
+  const desiredSplitIndexes = equalsIndexes
+    .map(equalsIndex => {
+      const lowerSpaceIndexes = splitIndexes.filter(i => i < equalsIndex);
+      return lowerSpaceIndexes.length === 0
+        ? false
+        : Math.max(...lowerSpaceIndexes);
+    })
+    .filter(Boolean);
+
+  const keyValuePairs = [];
+  desiredSplitIndexes.forEach((splitIndex, loopIndex) => {
+    const subStrStart =
+      loopIndex === 0 ? 0 : desiredSplitIndexes[loopIndex - 1];
+    keyValuePairs.push(data.substring(subStrStart, splitIndex));
+  });
+
+  keyValuePairs.push(
+    data.substring(
+      desiredSplitIndexes[desiredSplitIndexes.length - 1],
+      data.length
+    )
+  );
+
+  const parsed = {};
+
+  keyValuePairs.map(keyValuePair => {
+    const key = keyValuePair.split("=")[0].replace('"', "");
+    const value = (keyValuePair.split("=")[1] || "")
+      .replace('"', "")
+      .replace('"', ""); // dont ask
+    parsed[key] = value;
+  });
+
+  return parsed;
+};
+
+export default App;
