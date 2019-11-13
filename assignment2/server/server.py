a92dd3d31121ae7ce6143f4ed693f61b1a137ef4
Finn Julius Stephansen-Smith <finni-94@hotmail.com>
2019-11-13 13:36:03 +0100

    grep it twirl it drop it mix it

--- assignment2/server/server.py
+++ assignment2/server/server.py
@@ -123,8 +123,8 @@ def parse_snmp_request():
     command_to_get_sysName = 'snmpgetnext -v 2c -c ttm4128 localhost sysDescr'
     sys_name = os.popen(command_to_get_sysName).read()
 
-    command_to_get_IP_names = 'snmpwalk -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1 | grep IpAddress'  # TODO
-    command_to_get_IP_addresses = 'snmpwalk -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1 | grep IpAddress'
+    command_to_get_IP_names = 'snmpwalk -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1 | grep IpAddressAddr'  # TODO
+    command_to_get_IP_addresses = 'snmpwalk -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1 | grep IpAddressAddr'
     command_to_get_IP_mask = 'snmpwalk -v 2c -c ttm4128 localhost .1.3.6.1.2.1.4.20.1 | grep ipAdEntNetMask'
 
     ip_names_command_output = os.popen(command_to_get_IP_names).read()
@@ -135,9 +135,9 @@ def parse_snmp_request():
     print('snmp ip_addr_command_output', ip_addr_command_output)
     print('snmp ip_mask_command_output', ip_mask_command_output)
 
-    ip_names = ip_names_command_output.split('\n')
-    ip_addresses = ip_addr_command_output.split('\n')
-    ip_masks = ip_mask_command_output.split('\n')
+    ip_names = ip_names_command_output.split('\n')[:-1]
+    ip_addresses = ip_addr_command_output.split('\n')[:-1]
+    ip_masks = ip_mask_command_output.split('\n')[:-1]
 
     print('snmp ip_names', ip_names)
     print('snmp ip_addresses', ip_addresses)
